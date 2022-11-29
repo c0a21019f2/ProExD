420301ca1a7e056ebb4c797792b12a79c1c3f765
echigoya <c0a21019f2@edu.teu.ac.jp>
2022-11-29 14:34:10 +0900

    練習2-4

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 import tkinter.colorchooser as tkme
 
+
 #練習3
 def button_click(event):
     btn = event.widget
@@ -13,9 +14,12 @@ root = tk.Tk()
 root.title("calc.py")
 root.geometry("300x500")
 
+#練習4
+entry = tk.Entry(root, justify="right", width=10, font=("",40))
+entry.grid(row = 0, column=0, columnspan=3)
 
-#練習２
-r, c = 0, 0
+#練習2
+r, c = 1, 0
 for num in range(9, -1, -1):
     button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
@@ -28,7 +32,7 @@ for num in range(9, -1, -1):
 
 
         
-    
+
 
 
 root.mainloop()
