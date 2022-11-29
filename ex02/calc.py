dcbbde798c5b13c3c6702a3dd654e02eac03531b
echigoya <c0a21019f2@edu.teu.ac.jp>
2022-11-29 14:20:00 +0900

    練習2-2

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,7 +6,18 @@ root = tk.Tk()
 root.title("calc.py")
 root.geometry("300x500")
 
+#練習２
+r, c=0,0
+for num in range(9, -1, -1):
+    button = tk.Button(root, text=f"{num}", width=4, height=2,font=("",30)).pack()
+    button.grid(row = r, column = c)
+    c += 1
+    if c%3==0:
+        r+=1
+        c = 0
 
+        
+    
 
 
 root.mainloop()
