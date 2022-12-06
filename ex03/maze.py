133b1ae5ebcff68dbab644817943565e74b63804
echigoya <c0a21019f2@edu.teu.ac.jp>
2022-12-06 14:29:39 +0900

    練習３－４

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,7 +2,15 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 
-if _name__ == "__main__":
+if __name__ == "__main__":
+    key = ""
     root = tk.Tk()
     root.title("迷えるこうかとん")
+    canvas=tk.Canvas(root,width=1500,height=900, bg= "black")
+    canvas.pack()
+    
+    images = tk.PhotoImage(file="fig/8.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx, cy,image=images,tag = "koukaton")
     root.mainloop()
+    
