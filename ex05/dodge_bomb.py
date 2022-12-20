cc76b8a012dd183fc2af38e85ed06faaa08c9059
echigoya <c0a21019f2@edu.teu.ac.jp>
2022-12-20 14:55:42 +0900

    p

---
+++ ex05/dodge_bomb.py
@@ -0,0 +1,118 @@
+import pygame as pg
+import random
+import sys
+
+
+class Screen:
+    def __init__(self, title, wh, img_path):   
+        # 練習１
+        pg.display.set_caption(title)#"逃げろ！こうかとん"
+        self.sfc = pg.display.set_mode(wh)#(1600,900)
+        self.rct = self.rct.get_rect()
+        self.bgi_sfc = pg.image.load(img_path)#"fig/pg_bg.jpg"
+        self.bgi_rct = self.bgi_sfc.get_rect()
+        
+    def blit(self):
+        self.sfc.blit(self.bgi_sfc, self.bgi_rct) 
+
+class Bird:
+    key_delta = {
+        pg.K_UP:    [0, -1],
+        pg.K_DOWN:  [0, +1],
+        pg.K_LEFT:  [-1, 0],
+        pg.K_RIGHT: [+1, 0],
+    }
+
+    def __init__(self, img_path, ratio, xy):
+        # 練習３
+        self.sfc = pg.image.load(img_path)#"fig/6.png"
+        self.sfc = pg.transform.rotozoom(tori_sfc, 0, ratio)
+        self.rct = tori_sfc.get_rect()
+        self.rct.center = xy#900, 400
+        # scrn_sfcにtori_rctに従って，tori_sfcを貼り付ける
+    def blit(self, scr:Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+    
+    def update(self, scr:Screen):
+        key_dct = pg.key.get_pressed()
+        for key, delta in Bird.key_delta.items():
+            if key_dct[key]:
+                self.rct.centerx += delta[0]
+                self.rct.centery += delta[1]
+            if check_bound(self.rct, selfrct) != (+1, +1):     
+                self.rct.centerx -= delta[0]
+                self.rct.centery -= delta[1]
+        
+        self.blit(sfc)
+        
+
+            # 練習7
+            
+
+def check_bound(obj_rct, scr_rct):
+    """
+    第1引数：こうかとんrectまたは爆弾rect
+    第2引数：スクリーンrect
+    範囲内：+1／範囲外：-1
+    """
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = -1
+    return yoko, tate
+
+
+def main():
+    clock =pg.time.Clock()
+    
+    #練習1
+    scr = Screen("逃げろ！こうかとん", (1600,900), "fig/pg_bg.jpg")
+
+    #練習3
+    kkt = Bird("fig/6.png", 2.0, (900,400))
+    kkt.blit(scr)#    scrn_sfc.blit(bomb_sfc, bomb_rct) 
+    
+    # 練習５
+    bomb_sfc = pg.Surface((20, 20)) # 正方形の空のSurface
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct.centery = random.randint(0, scrn_rct.height)
+    
+    vx, vy = +1, +1
+
+    # 練習２
+    while True:
+        Screen.blit()# scrn_sfc.blit(pgbg_sfc, pgbg_rct) 
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
+        
+        kkt.blit(scr) # 練習3
+
+        # 練習６
+        bomb_rct.move_ip(vx, vy)
+        scrn_sfc.blit(bomb_sfc, bomb_rct) 
+        yoko, tate = check_bound(bomb_rct, scrn_rct)
+        vx *= yoko
+        vy *= tate
+
+        # 練習８
+        if tori_rct.colliderect(bomb_rct):
+            return
+
+        pg.display.update()
+        clock.tick(1000)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
+
+
+
